53d24c18ef610d546283dc4b6cf4c642ad32e53f
Alexandru Adam <adamalexandru4@yahoo.com>
2020-04-12 02:37:23 +0300

    Copy from old repo - V2

---
+++ app/src/main/java/com/ethvotingverifier/SplashScreenActivity.java
@@ -0,0 +1,25 @@
+package com.ethvotingverifier;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreenActivity extends AppCompatActivity {
+    private static int TIME_OUT = 4000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreenActivity.this, StartScreenActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, TIME_OUT);
+    }
+}
---
+++ app/src/main/java/com/ethvotingverifier/wallet/ImportWalletActivity.java
@@ -0,0 +1,16 @@
+package com.ethvotingverifier.wallet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ethvotingverifier.R;
+
+public class ImportWalletActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_load_wallet);
+    }
+}
