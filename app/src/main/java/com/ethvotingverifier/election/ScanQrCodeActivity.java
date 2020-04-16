94943266dd33c6d9aeff50e3f3c7dce3118aba1e
Alexandru Adam <adamalexandru4@yahoo.com>
2020-04-17 00:01:13 +0300

    Scan QR fixed. Working 100%

--- app/src/main/java/com/ethvotingverifier/election/ScanQrCodeActivity.java
+++ app/src/main/java/com/ethvotingverifier/election/ScanQrCodeActivity.java
@@ -1,9 +1,13 @@
 package com.ethvotingverifier.election;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 import com.ethvotingverifier.R;
@@ -19,6 +23,10 @@ public class ScanQrCodeActivity extends AppCompatActivity implements ZXingScanne
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 123);
+        }
 
         scannerView = new ZXingScannerView(this);
         setContentView(scannerView);
